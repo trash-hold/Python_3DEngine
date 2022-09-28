6003fa31837a747e855351239ad832241cc5edab
B-Ash <ash_tyle@protonmail.com>
2022-09-28 18:19:15 +0200

    git

--- main.py
+++ main.py
@@ -1,61 +1,75 @@
+"""
 from logging import raiseExceptions
 import numpy as np
 import math as m
-from shapes import Circle
-
+from camera import Camera
+from shapes import Circle, Donut
+from main import Window
+from renderer import Renderer
+from tkinter import *
 __frame_size__ = 100 
-class Window:
-    def __init__(self, w_sizeX = 60, w_sizeY = 60, win_rules = ["center", "adjust"], shapes = None, obj = None, init = False):
-        """
-        w_size - width and height of render in terminal; int > 1
-        obj - matrix of points' coordinates (x,y,z) in a form of matrix size (N, 3) 
-        win_rules - set of rules which define how camera is gonna work; must be dictionary keys
-        """
-        #Private variables
-        self.__wsize__ = None
-        self.__shapes__ = None
-        self.__obj__ = None 
-        self.__rules__ = {"center": False, "adjust": False, "z_red": False}        
 
-        if (init == False):
-            #Intialising window size vector 
-            if isinstance(w_sizeX, int) and isinstance(w_sizeY, int):
-                if w_sizeX > 1 and w_sizeY > 1: self.__wsize__ = np.array([w_sizeX, w_sizeY, 1]).T
-                else: raise ValueError("ErrV2: Window size can't be smaller than 1px")
-            else:
-                    #raise ValueError("ErrV1: Window sizes must be integers")
-                    print("Problem with remaking it into vector after inheriting from child")
+if __name__ == "__main__":
+    # PROBLEMS WITH ZERO ARRAY IN RENDER()
+    #PROBLEM WITH ZERO ARRAY IN RASTERIZATION
 
-            #SHAPES IN PROGRESS
+    #mat = np.random.randint(10, size = (3, 20))
+    #mat = np.array([[4, 4, 4, 4],[4, 4, 4, 4],[3, 8, 7, 1]])
+    #mat = np.array([[20, 40, 50],[0, 20, -10],[1, 1, 1]])
+    mat = np.array([0, 0, 0])
+    #print("The random matrix:")
+    #print(mat)
+    t = np.zeros(shape = (2, 3))
+    par = np.array([[0], [0], [-10]])
+    rot = np.array([m.radians(0), m.radians(0), m.radians(0)])
+    trans = np.column_stack((par, rot))
 
-            #Initialising object matrix
-            if obj is not None:
-                if isinstance(obj, np.ndarray): 
-                    if np.shape(obj)[0] == 3: self.__object__ = obj
-                    else: raise ValueError("ErrV4: Input matrix has wrong data format")
-                else: raise ValueError("ErrV3: Input object isn't in matrix form")
+    w = Window(60, 60)
+    #c = Circle([300])
+    c = Donut([300, 100])
+    w.update_obj(c.__obj__)
+    #print(c.__obj__)
+    cam = Camera(w)
+    red = Renderer(cam)
+    #red.render()
 
-            #Initialising rule set
-            if isinstance(win_rules, str): win_rules = [win_rules]
-            elif win_rules is None: win_rules = []
-            for i in win_rules:
-                if i in self.__rules__.keys(): self.__rules__[i] = True
-                else: raise ValueError("ErrV4: Non existing key word for window")
-        else:
-            self.__wsize__ = np.array([w_sizeX, w_sizeY, 1])
-            self.__shapes__ = shapes
-            self.__obj__ = obj 
-            self.__rules__ = win_rules 
-        
-        #Public variables
-        self.camera = None
-        self.renderer = None
-    
-    def update_obj(self, obj):
-        self.__obj__ = obj
+    #red.animate()
+    #red.win.mainloop()
+    #print(100*"# ")
+    #for i in range(100): print("#")
 
-    def get_obj(self):
-        return self.__obj__
+    win = Tk()
 
-    def get_wsize(self):
-        return self.__wsize__
+
+
+
+    Issue: mode="adjust" weird rotation results especially for 0, 90, 0, see differences between mode = adjust + center
+    Issue: centering fucking sucks B) - SOLVED
+
+    """
+
+import engine as e
+import gui as g
+
+if __name__ == "__main__":
+    w = e.Window(60,60)
+    c = e.Donut([300, 100])
+    w.update_obj(c.__obj__)
+    g = g.GUI(w)
+    #g.donut()
+
+
+    """
+    settings:
+	Window - resolution
+	res lock - on/off?
+	dark theme - meme
+	
+graphics:
+	frame_size - 
+	window_size - make cap depending on res???
+	camera settings on/off:
+		adjust
+		center
+		z-depth
+	"""
